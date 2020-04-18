616f884f98627c0c6fa94c0a438f34e454f02ca5
Danielle Schugars <danielle.schugars@gmail.com>
2020-04-17 23:08:07 -0700

    add log object

---
+++ Assets/ScriptableObjects/Pickups/ConsumeLog.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ConsumeLog.asset", menuName = "Consumables/Log")]
+public class ConsumeLog : ConsumeAction{
+
+    public override void Apply(PlayerController player)
+    {
+        player.AddFuel(fuel);
+    }
+}
