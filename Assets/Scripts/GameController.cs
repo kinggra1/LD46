da154470d777c558065d6f77c1ad8e95eff48810
Danielle Schugars <danielle.schugars@gmail.com>
2020-04-18 13:13:13 -0700

    add GameController

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GameController : MonoBehaviour
 {
+    private PlayerController player;
+    private Grid grid;
+    private Tilemap tilemap;
+
+    private TileBase burntGrassTile;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindObjectOfType<PlayerController>();
+        grid = GameObject.FindObjectOfType<Grid>();
+        tilemap = GameObject.FindObjectOfType<Tilemap>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckTileUnderPlayer();
+    }
+
+    private void CheckTileUnderPlayer()
+    {
+        Vector3Int cellPosition = grid.WorldToCell(player.gameObject.transform.position);
+        TileBase tile = tilemap.GetTile(cellPosition);
+        if (tile.name == "grass")
+        {
+            Debug.Log("grass");
+        }
     }
 }
---
+++ Assets/Scripts/GrassTile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "GrassTile.asset", menuName = "Tiles/Grass")]
+public class GrassTile : Tile
+{
+    public Sprite Grass;
+    public Sprite BurntGrass;
+
+    private bool burnt = false;
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        base.GetTileData(position, tilemap, ref tileData);
+
+        tileData.sprite = burnt ? BurntGrass : Grass;
+    }
+
+    public void Burn()
+    {
+        burnt = true;
+    }
+
+    public bool IsBurnt()
+    {
+        return burnt;
+    }    
+
+}
