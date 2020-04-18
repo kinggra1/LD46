f6f3e8e29c561a22899d900e65c34b06ce1eed98
Grant King <alpharock6@gmail.com>
2020-04-17 23:51:45 -0700

    Added demo Tilemap and working on collisions

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
     private PlayerController player;
+    private Camera camera;
 
     private float zOffset = -5f;
 
     // Start is called before the first frame update
     void Start() {
         player = GameObject.FindObjectOfType<PlayerController>();
+        camera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update() {
+        camera.orthographicSize = GetZoomDist();
         Vector3 cameraOffset = new Vector3(0f, 0f, zOffset);
         this.transform.position = player.transform.position + cameraOffset;
+    }
+
+    private float GetZoomDist() {
+        return Mathf.Clamp(player.GetFuel(), 1, 20) / 2;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,10 @@ public class PlayerController : MonoBehaviour
     // fuel lost per second
     private float fuelLossRate = 0.5f;
 
+
+    private float xInput;
+    private float yInput;
+
     // Start is called before the first frame update
     void Start() {
         fuelLossRate = DEFAULT_FUEL_LOSS_RATE;
@@ -19,12 +23,15 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        float xInput = Input.GetAxis("Horizontal");
-        float yInput = Input.GetAxis("Vertical");
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
 
         fuel -= fuelLossRate * Time.deltaTime;
+    }
 
-        transform.position += new Vector3(xInput, yInput) * MOVEMENT_SPEED * Time.deltaTime;
+    // Physics timestep. Put motion code here for smoother motions and collisions.
+    private void FixedUpdate() {
+        transform.position += new Vector3(xInput, yInput) * MOVEMENT_SPEED * Time.fixedDeltaTime;
         transform.localScale = new Vector3(fuel, fuel);
     }
 
@@ -33,12 +40,16 @@ public class PlayerController : MonoBehaviour
         // TODO: Check for constraints (e.g. 0 since this could be negative fuel).
     }
 
+    public float GetFuel() {
+        return fuel;
+    }
+
     // Callback whenever we enter a "trigger", which is a collder on an object with "isTrigger" checked.
-    private void OnTriggerEnter2D(Collider2D other) {
-        ConsumableObject consumable = other.GetComponent<ConsumableObject>();
-        if (consumable) {
-            consumable.BeConsumedBy(this);
-        }
+    private void OnCollisionEnter2D(Collision2D collision) {
+        ConsumableObject consumable = collision.otherCollider.GetComponent<ConsumableObject>();
+            if (consumable) {
+                consumable.BeConsumedBy(this);
+            }
         
     }
 }
