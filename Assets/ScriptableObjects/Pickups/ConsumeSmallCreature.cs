0b3468cdd44a14dfc372ba225994f1f5a6b6473c
Grant King <alpharock6@gmail.com>
2020-04-19 15:44:54 -0700

    Little squirrels running around

---
+++ Assets/ScriptableObjects/Pickups/ConsumeSmallCreature.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ConsumeSmallCreature.asset", menuName = "Consumables/SmallCreature")]
+public class ConsumeSmallCreature : ConsumeAction{
+
+    public override void ApplyTo(PlayerController player)
+    {
+        player.AddFuel(fuel);
+        // TODO: Small animal *BIP* sound
+    }
+}
