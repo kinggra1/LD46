2bc02f2d1298290bba4c25a9f7d063f447c99629
Danielle Schugars <danielle.schugars@gmail.com>
2020-04-19 18:11:32 -0700

    add bushes

---
+++ Assets/ScriptableObjects/Pickups/ConsumeBush.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ConsumeBush.asset", menuName = "Consumables/Bush")]
+public class ConsumeBush : ConsumeAction{
+    public override void ApplyTo(PlayerController player)
+    {
+        player.AddFuel(fuel);
+    }
+}
